61a6780b173ff00af9e7767660ded77b1493d517
NatalmanNahm <natalnahm@gmail.com>
2021-04-27 19:06:19 -0500

    Modified the main activity to add more buttons and text.

--- app/src/main/java/com/example/roadtrip_fwanddp/MainActivity.java
+++ app/src/main/java/com/example/roadtrip_fwanddp/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
@@ -16,15 +17,35 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         FloatingActionButton addLocFab = findViewById(R.id.add_fab);
+        addLocFab.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, AddLocationActivity.class);
+            startActivity(intent);
 
-        addLocFab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, AddLocationActivity.class);
+        });
 
-                startActivity(intent);
+        Button addCurBtn = findViewById(R.id.addCurLocBtn);
+        Button addOpt1Btn = findViewById(R.id.addOptOneBtn);
+        Button addOpt2Btn = findViewById(R.id.addOptTwoBtn);
+        Button addOpt3Btn = findViewById(R.id.addOptThreeBtn);
 
-            }
+        addCurBtn.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, AddLocationActivity.class);
+            startActivity(intent);
+        });
+
+        addOpt1Btn.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, AddLocationActivity.class);
+            startActivity(intent);
+        });
+
+        addOpt2Btn.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, AddLocationActivity.class);
+            startActivity(intent);
+        });
+
+        addOpt3Btn.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, AddLocationActivity.class);
+            startActivity(intent);
         });
     }
 }
